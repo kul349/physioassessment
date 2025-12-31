0d0e212b9975ada4e35bbc8dc66ebccc332b0344
kul349 <karkikulbahadur344@gmail.com>
2025-12-31 11:14:49 +0545

    adding thefile

---
+++ src/app/robots.js
@@ -0,0 +1,11 @@
+// src/app/robots.js
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/api/", "/_next/"],
+    },
+    sitemap: "https://physioassessment.vercel.app/sitemap.xml",
+  };
+}
